b02d567c71bbff0b6445449aebcdbcb0c7ea950d
Ishu Singh <104965112+ishuoncode@users.noreply.github.com>
2023-12-16 16:30:06 +0530

    dfdsfddffd

--- app/api/cron/route.tsx
+++ app/api/cron/route.tsx
@@ -13,7 +13,7 @@ import { NextResponse } from "next/server";
 
 
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+// export const maxDuration = 300; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
